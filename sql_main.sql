4e933b812b8548950bfc74830ef43a9fd9228756
rjsah7790-design <rjsah7790@gmail.com>
2025-09-03 15:54:25 +0900

    testcommit

--- sql_main.sql
+++ sql_main.sql
@@ -1,12 +1,4 @@
 use wakutabi;
 create table user(
-	id bigint auto_increment primary key,
-    username varchar(50) not null unique,
-    password varchar(100) not null,
-    name varchar(50) not null,
-    email varchar(100) not null unique,
-    phone varchar(20),
-    gender enum('mail','female','other') not null default 'orther',
-    created_at datetime default current_timestamp,
-    updated_at datetime default current_timestamp on update current_timestamp
+	
 );

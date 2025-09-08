f6d13381dee4170efdf92bd1963c1561eccdbebc
MG Kim <canon2440@gmail.com>
2025-09-08 16:59:43 +0900

    Modify: 사용자 테이블 birth 칼럼 NOT NULL 제약 조건 추가, introduce 칼럼 NULL 제약 조건 제거

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,13 +5,13 @@ CREATE TABLE users (
     username   VARCHAR(20)                          NOT NULL UNIQUE,                      -- 사용자ID
     nickname   VARCHAR(20)                          NOT NULL UNIQUE,                      -- 닉네임(미입력 시 사용자ID)
     password   VARCHAR(256)                         NOT NULL,                             -- 비밀번호
-    gender     ENUM('MALE','FEMALE','OTHER','NONE')          DEFAULT 'NONE',              -- 성별
+    gender     ENUM('MALE','FEMALE','OTHER','NONE') NOT NULL DEFAULT 'NONE',              -- 성별
     birth      DATETIME                             NOT NULL,                             -- 생일
     email      VARCHAR(255)                         NOT NULL UNIQUE,                      -- 이메일
     is_public  BOOLEAN                              NOT NULL DEFAULT TRUE,                -- 공개여부
     role       ENUM('USER','ADMIN')                 NOT NULL DEFAULT 'USER',              -- 사용자권한('USER': 일반사용자, 'ADMIN': 관리자)
     status     ENUM('ACTIVE','SUSPENDED','BANNED')  NOT NULL DEFAULT 'ACTIVE',            -- 계정상태('ACTIVE':활성화, 'SUSPEND':일시정지, 'BANNED':영구정지)
-    introduce  TEXT                                     NULL,                             -- 자기소개글
+    introduce  TEXT,                                                                      -- 자기소개글
     created_at DATETIME                             NOT NULL DEFAULT CURRENT_TIMESTAMP,   -- 생성일자
     updated_at DATETIME                                      DEFAULT CURRENT_TIMESTAMP    -- 수정일자
 );

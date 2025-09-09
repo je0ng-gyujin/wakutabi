337c455368ff649f8998c63c59ad0e74e8b78250
rjsah7790-design <rjsah7790@gmail.com>
2025-09-09 15:05:04 +0900

    회원가입할때 아이디중복체크

--- src/main/java/com/wakutabi/controller/UserController.java
+++ src/main/java/com/wakutabi/controller/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wakutabi.domain.UserDto;
 import com.wakutabi.service.UserService;
@@ -21,6 +23,17 @@ public class UserController {
 	public String signRegister(UserDto user) {
 		userService.register(user);
 		return "redirect:/";
+	}
+	@GetMapping("/check-username")
+	@ResponseBody
+	public String checkUsername(@RequestParam("username") String username) {
+		try {
+			int count = userService.countByusername(username);
+			return count > 0 ? "exist" : "ok";
+		}catch(Exception e){
+			e.printStackTrace();
+			return "error";
+		}
 	}
 	
 }

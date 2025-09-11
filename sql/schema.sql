af67eb65a8593a7afd7a245d739518240bce1207
MG Kim <canon2440@gmail.com>
2025-09-11 16:21:33 +0900

    Modify: users, trip_article Table 수정

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,12 +8,13 @@ CREATE TABLE users (
     gender     ENUM('MALE','FEMALE','OTHER','NONE') NOT NULL DEFAULT 'NONE',              -- 성별
     birth      DATETIME                             NOT NULL,                             -- 생일
     email      VARCHAR(255)                         NOT NULL UNIQUE,                      -- 이메일
-    is_public  BOOLEAN                              NOT NULL DEFAULT TRUE,                -- 공개여부
+    is_public  BOOLEAN                              NOT NULL DEFAULT TRUE,                -- 공개여부(기본 공개)
     role       ENUM('USER','ADMIN')                 NOT NULL DEFAULT 'USER',              -- 사용자권한('USER': 일반사용자, 'ADMIN': 관리자)
     status     ENUM('ACTIVE','SUSPENDED','BANNED')  NOT NULL DEFAULT 'ACTIVE',            -- 계정상태('ACTIVE':활성화, 'SUSPEND':일시정지, 'BANNED':영구정지)
     introduce  TEXT,                                                                      -- 자기소개글
     created_at DATETIME                             NOT NULL DEFAULT CURRENT_TIMESTAMP,   -- 생성일자
-    updated_at DATETIME                                      DEFAULT CURRENT_TIMESTAMP    -- 수정일자
+    updated_at DATETIME                                      DEFAULT CURRENT_TIMESTAMP,   -- 수정일자
+    deleted_at DATETIME                                                                   -- 삭제(탈퇴)일자
 );
 -- 사용자 선호도
 CREATE TABLE user_prefer (
@@ -27,10 +28,11 @@ CREATE TABLE user_prefer (
 
 	CONSTRAINT fk_user_prefer_user_id FOREIGN KEY (user_id) REFERENCES users (id)
 );
--- 여행등록 관련
+-- [여행등록 관련]
+-- 여행일정 등록
 CREATE TABLE trip_article (
 	id	               BIGINT                                         AUTO_INCREMENT PRIMARY KEY,                                             -- 여행일정게시 고유ID
-	host_user_id	   BIGINT                                         NOT NULL,	                                                              -- 게시자_사용자ID
+	host_user_id	   BIGINT                                         NOT NULL,	                                                              -- 주최자 고유ID
 	location           VARCHAR(10)   	                              NOT NULL,	                                                              -- 지역
 	start_date	       DATETIME	                                      NOT NULL,                                                               -- 여행시작날짜
 	end_date	       DATETIME	                                      NOT NULL,                                                               -- 여행종료날짜
@@ -43,8 +45,9 @@ CREATE TABLE trip_article (
 	status	           ENUM('OPEN','MATCHED','CLOSED','CANCELED')     NOT NULL DEFAULT 'OPEN',                                                -- 진행상태('OPEN':진행중,'MATCHED':매치완료,'CLOSED':매치종료,'CANCELED':매치취소)
     created_at         DATETIME                                       NOT NULL DEFAULT CURRENT_TIMESTAMP,                                     -- 생성일자
     updated_at         DATETIME                                                DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,         -- 수정일자
-    deleted_at         DATETIME                                           NULL,                                                               -- 삭제일자
-	CONSTRAINT fk_trip_article_user_id     FOREIGN KEY (host_user_id) REFERENCES users (id)
+    deleted_at         DATETIME,                                                                                                              -- 삭제일자
+
+	CONSTRAINT fk_trip_article_host_user_id FOREIGN KEY (host_user_id) REFERENCES users (id)
 );
 -- 여행후기 관련
 

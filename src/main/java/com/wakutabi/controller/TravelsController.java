30b5180bbf8e9b088080e883de8d69b6335f26c1
rjsah7790-design <rjsah7790@gmail.com>
2025-09-17 17:18:34 +0900

    이미지저장되는데 여러장이안돼요

--- src/main/java/com/wakutabi/controller/TravelsController.java
+++ src/main/java/com/wakutabi/controller/TravelsController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;

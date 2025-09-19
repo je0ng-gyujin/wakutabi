9d7818b1533958b1783411f3b80332b54e3a256e
kshsh234 <xmxm134@gmail.com>
2025-09-19 18:31:42 +0900

    회원정보 수정controller, service, mapper 구현

--- src/main/java/com/wakutabi/mapper/UserMapper.java
+++ src/main/java/com/wakutabi/mapper/UserMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Param;
 
 import com.wakutabi.domain.LoginDto;
 import com.wakutabi.domain.SignUpDto;
+import com.wakutabi.domain.UserUpdateDto;
 @Mapper
 public interface UserMapper {
 		//username검색
@@ -13,4 +14,9 @@ public interface UserMapper {
 		int insertUser(SignUpDto user);
 		//로그인 처리
 		LoginDto findByUsername(String username);
+		
+		//회원정보 갖고오기
+		UserUpdateDto getUserInfo(String username);
+		//회원정보수정(유저 정보수정)
+		void userInfoUpdate(UserUpdateDto user);
 }

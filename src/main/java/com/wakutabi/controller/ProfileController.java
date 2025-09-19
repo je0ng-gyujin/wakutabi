4d06afa8adc0b5b45df8d105e82255191d6ed6ec
kshsh234 <xmxm134@gmail.com>
2025-09-19 15:42:22 +0900

    html 연결

--- src/main/java/com/wakutabi/controller/ProfileController.java
+++ src/main/java/com/wakutabi/controller/ProfileController.java
@@ -13,33 +13,5 @@ public class ProfileController {
     public String enterProfile(){
         return "infos/profile";
     }
-     @GetMapping("/mypage")
-    public String enterMypage(){
-        return "infos/mypage";
-    }
-     @GetMapping("/my-Inquiries")
-    public String enterMyInquiries(){
-        return "infos/my-Inquiries";
-    }
-     @GetMapping("/info")
-    public String enterInfo(){
-        return "infos/info";
-    }
-     @GetMapping("/user-inquiry")
-    public String enterUserInquiry(){
-        return "infos/user-inquiry";
-    }
-     @GetMapping("/change-password")
-    public String enterChangePassword(){
-        return "infos/change-password";
-    }
-     @GetMapping("/delete")
-    public String enterDelete(){
-        return "infos/delete";
-    }
-     @GetMapping("/inquiry-read")
-    public String enterInquiryRead(){
-        return "infos/inquiry-read";
-    }
 
 }

5fbcc7e4d2075b11e6c812e2a058afcf4228ae09
Se0ngChan <tjdcks0692@gmail.com>
2025-09-19 17:16:31 +0900

    임시저장

--- src/main/java/com/wakutabi/service/TravelImageService.java
+++ src/main/java/com/wakutabi/service/TravelImageService.java
@@ -1,5 +1,7 @@
 package com.wakutabi.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.wakutabi.domain.TravelImageDto;
@@ -14,4 +16,9 @@ public class TravelImageService {
 	public void insertTravelImage(TravelImageDto dto) {
 		travelImageMapper.insertTravelImage(dto);
 	}
+	
+	// 이 메서드를 추가하여 이미지 목록을 조회합니다.
+    public List<TravelImageDto> findImagesByTripArticleId(Long tripArticleId) {
+        return travelImageMapper.findByTripArticleId(tripArticleId);
+    }
 }

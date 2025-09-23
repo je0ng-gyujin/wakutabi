985ee2043bb856b172174915e85a162883e537dd
kshsh234 <xmxm134@gmail.com>
2025-09-23 16:12:45 +0900

    ADD : 여행참가파일

---
+++ src/main/java/com/wakutabi/controller/TravelJoinRequestController.java
@@ -0,0 +1,15 @@
+package com.wakutabi.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+
+@Controller
+@RequiredArgsConstructor
+public class TravelJoinRequestController {
+
+    
+}

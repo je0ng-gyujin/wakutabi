822cf664ac0130898448122c10388847fec65b24
kshsh234 <xmxm134@gmail.com>
2025-10-01 17:45:15 +0900

    탈퇴한 계정 로그인 금지처리

--- src/main/java/com/wakutabi/service/CustomUserDetailService.java
+++ src/main/java/com/wakutabi/service/CustomUserDetailService.java
@@ -1,5 +1,6 @@
 package com.wakutabi.service;
 
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,12 +17,16 @@ import lombok.RequiredArgsConstructor;
 public class CustomUserDetailService implements UserDetailsService  {
 
 	private final UserMapper userMapper;
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		LoginDto dto = userMapper.findByUsername(username);
 		if(dto==null) {
 			throw new UsernameNotFoundException("사용자를 찾을 수 없다"+username);
+		}
+		if("EXIT".equalsIgnoreCase(dto.getStatus())){
+			System.out.println("status 값: " + dto.getStatus());
+			throw new DisabledException("탈퇴한 계정입니다.");
 		}
 			return User.builder()
 					.username(dto.getUsername())

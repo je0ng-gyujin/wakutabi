c09a15750f4bad0c5e75de3ad993fe6e21f0dbf2
kowaicode <canon2440@gmail.com>
2025-10-28 21:22:35 +0900

    상세페이지 코드 정리 및 상태 표시 로직 세분화, 서비스 클래스명에 서비스 추가, MainController 내 불필요한 경로 메서드 제거하였슨 (#74)
    * fix: 여행 상태가 모집 마감일에 조기 종료되는 문제 수정 및 스케줄러 서비스명 변경
    - 클래스명 혼동 방지를 위해 Service 접미사 추가
    - 모집 마감일이 아닌 여행 종료일로 기준 변경하여 여행 상태가 조기 종료되는 문제 수정
    
    * feat: 상세페이지 코드 정리 및 상태 표시 로직 세분화, MainController 내 불필요한 경로 메서드 제거
    - 여행 종료일이 지나지 않았음에도 '여행 종료' 상태가 표시되는 문제 수정
    - MainController에서 detail 경로 메서드 제거로 상세페이지 경로 접근 혼동 해소
    - 상세페이지 html 코드 정리로 가독성 Up Up Up

--- src/main/java/com/wakutabi/service/TravelCanceledAndEndSchedulerService.java
+++ src/main/java/com/wakutabi/service/TravelCanceledAndEndSchedulerService.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class TravelCanceledAndEndScheduler {
+public class TravelCanceledAndEndSchedulerService {
 
     private final TravelCanceledAndEndMapper travelCanceledAndEndMapper;
     private final ChatParticipantsMapper chatParticipantsMapper;
--- src/main/java/com/wakutabi/service/TravelDeadlineSchedulerService.java
+++ src/main/java/com/wakutabi/service/TravelDeadlineSchedulerService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class TravelDeadlineScheduler {
+public class TravelDeadlineSchedulerService {
 
     private final TravelDeadlineMapper travelDeadlineMapper;
 
